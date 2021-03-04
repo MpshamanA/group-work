0ba9337e2afa260b774f758349febe1d592b2a48
shoIwasaki <>
2021-03-05 01:05:30 +0900

    検索機能作成中

--- StoreSearch/app.js
+++ StoreSearch/app.js
@@ -80,58 +80,105 @@ const shopList = [
     }
 ];
 
-for(let i = 0; i < shopList.length;i++){
+let shop = document.getElementById('shop');
 
+for (let i = 0; i < shopList.length; i++) {
+    let div = document.createElement('div');
+    div.className = 'remove';
     let ul = document.createElement('ul');
-    /*イメージを取得*/
+    ul.className = 'list';
+    //イメージを取得
     let img = document.createElement('img');
-    img.setAttribute('src',shopList[i].image);
+    img.setAttribute('src', shopList[i].image);
 
     let contents = document.createElement('div');
     contents.className = 'contents';
 
-    let liC= document.createElement('li');
+    let liC = document.createElement('li');
     liC.className = 'shop-category';
     liC.innerHTML = shopList[i].category;
 
-    let liN= document.createElement('li');
+    let liN = document.createElement('li');
     liN.className = 'shop-name';
     liN.innerHTML = shopList[i].name;
 
-    let liA= document.createElement('li');
+    let liA = document.createElement('li');
     liA.className = 'shop-address';
     liA.innerHTML = shopList[i].address
 
-    let liS= document.createElement('li');
+    let liS = document.createElement('li');
     liS.className = 'shop-special';
     liS.innerHTML = shopList[i].special
 
-    let shop = document.getElementById('shop');
-    shop.appendChild(ul);
+    /* shop.appendChild(ul);
+    ul.appendChild(img);
+    contents.appendChild(liC);
+    contents.appendChild(liN);
+    contents.appendChild(liA);
+    contents.appendChild(liS);
+    ul.appendChild(contents); */
+
+    shop.appendChild(div);
     ul.appendChild(img);
     contents.appendChild(liC);
     contents.appendChild(liN);
     contents.appendChild(liA);
     contents.appendChild(liS);
     ul.appendChild(contents);
-    /* shops.appendChild(contents);
-    contents,appendChild(liC);
-    contents,appendChild(liN);
-    contents,appendChild(liA);
-    contents,appendChild(liS); */
+    div.appendChild(ul);
 };
 
 const keyword = document.getElementById("keyword");
 const searchbtn = document.getElementById('searchbtn');
 
-/*from内の検索キーワードを取得しキーワードを含む値を表示*/
-function onButtonClick(){
+function onButtonClick() {
+    //form内の検索ワード取得
     let key = document.forms.search.keyword.value;
-   const test = shopList.filter((shop) => {
-       return (shop.category.match(key));
-   });
-};
+    //検索ワードが含まれるデータを変数に代入
+    const test = shopList.filter((shop) => {
+        return (shop.category.match(key));
+    });
+    
+    if (key !== "") {
+        const uls = document.getElementsByClassName('list');
+        for (let i = 0; i < test.length; i++) {
+            let ul = document.createElement('ul');
+            ul.className = 'list';
+            //イメージを取得
+            let img = document.createElement('img');
+            img.setAttribute('src', test[i].image);
 
+            let contents = document.createElement('div');
+            contents.className = 'contents';
+
+            let liC = document.createElement('li');
+            liC.className = 'shop-category';
+            liC.innerHTML = test[i].category;
+
+            let liN = document.createElement('li');
+            liN.className = 'shop-name';
+            liN.innerHTML = test[i].name;
+
+            let liA = document.createElement('li');
+            liA.className = 'shop-address';
+            liA.innerHTML = test[i].address
+
+            let liS = document.createElement('li');
+            liS.className = 'shop-special';
+            liS.innerHTML = test[i].special
+
+            shop.appendChild(ul);
+            ul.appendChild(img);
+            contents.appendChild(liC);
+            contents.appendChild(liN);
+            contents.appendChild(liA);
+            contents.appendChild(liS);
+            ul.appendChild(contents);
+        };
+
+    };
+
+};
 
 /* const app = new Vue({
     el: "#app",
